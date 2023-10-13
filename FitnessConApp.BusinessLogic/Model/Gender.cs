c7c2ffa59dc68d81ab9a8252ca5ab74146ef9440
Закир Алекперов <zakiralekperov1998@gmail.com>
2023-10-13 23:05:46 +0300

    Создана модельь пользователя

---
+++ FitnessConApp.BusinessLogic/Model/Gender.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;
+
+namespace FitnessConApp.BusinessLogic.Model
+{
+    /// <summary>
+    /// Пол
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название пола
+        /// </summary>
+        public string Name { get;}
+
+
+        /// <summary>
+        /// Создать новый пол, сгенерировать исключение, если назване пола пустое или null.
+        /// </summary>
+        /// <param name="name">Название пола</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
